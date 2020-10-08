63dd0d64f0c757eab406b7cce1eadb7f2b1648b7
Venkata Naga Saraswathi Dronamraju <Vdronamraju3770@conestogac.on.ca>
2020-10-08 02:32:11 -0400

    Testing the Switch loop

---
+++ CheckTriangle.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Triangle
+{
+    public class CheckTriangle
+    {
+        public static void Main()
+        {
+            int opt;
+            Console.WriteLine("\n\n");
+            Console.WriteLine("Choose the options below:");
+            Console.WriteLine("\n\n");
+            Console.WriteLine("1) Enter the Triangle Dimensions");
+            Console.WriteLine("\n\n");
+            Console.WriteLine("2) Exit");
+            Console.WriteLine("\n\n");
+            opt = Convert.ToInt32(Console.ReadLine());
+
+            switch (opt)
+            {
+                case 1:
+
+                    Console.WriteLine("To Find if the given dimensions of the triangle is an equilateral, Isosceles, Scalene.");
+                    Console.WriteLine("\n\n");
+
+                    Console.WriteLine("Provide the value for side 1 of the triangle");
+                   int sidex = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Provide the value for side 2 of the triangle");
+                   int sidey = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Provide the value for side 3 of the triangle");
+                   int sidez = int.Parse(Console.ReadLine());
+
+                    var CheckTriangle = new CheckTriangle();
+                        Console.WriteLine(TriangleSolver.Analyze(sidex, sidey, sidez));
+                    break;
+
+                case 2:
+                    break;
+
+                default:
+                    Console.WriteLine("Enter the correct option");
+                    break;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Triangle
+{
+    public class TriangleSolver
+    {
+
+        public static string Analyze(int sidex, int sidey, int sidez)
+        {
+            if (sidex == sidey && sidey == sidez)
+            {
+                return "It is a equilateral triangle.\n";
+            }
+            else if (sidex == sidey || sidey == sidez || sidex == sidez)
+            {
+                return "It is a isosceles triangle.\n";
+            }
+            else
+            {
+                return "It is a scalene triangle.\n";
+            }
+
+        }
+    }
+}
